f5752a0bc613677d45a81749614a2e8ec76d27d7
Kozhin Valeriy <35922956+jeanedit@users.noreply.github.com>
2023-07-16 23:01:00 +0500

    11th sprint

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -5,11 +5,10 @@
 
 
 int main() {
-	tr_catalogue::TransportCatalogue transport_catalogue;
-	tr_catalogue::json_reader::JsonReader jr(transport_catalogue,std::cin);
+    tr_catalogue::TransportCatalogue transport_catalogue;
+    tr_catalogue::json_reader::JsonReader jr(transport_catalogue,std::cin);
     jr.ReadJson();
     renderer::MapRenderer mr = jr.MapRenderFromJson();
     tr_catalogue::RequestHandler rq(transport_catalogue,mr);
     jr.ResponseRequests(std::cout,rq);
-    //rq.RenderMap().Render(std::cout);
 }
