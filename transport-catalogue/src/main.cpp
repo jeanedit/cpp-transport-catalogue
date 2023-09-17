ef119db1618d57ba31d17ce295bead1d6aa1eaea
Valery Kozhin <35922956+jeanedit@users.noreply.github.com>
2023-09-17 19:40:31 +0500

    15th sprint

--- transport-catalogue/src/main.cpp
+++ transport-catalogue/src/main.cpp
@@ -1,17 +1,3 @@
-#include <iostream>
-
-
-//
-//int main() {
-//    tr_catalogue::TransportCatalogue transport_catalogue;
-//    tr_catalogue::json_reader::JsonReader jr(transport_catalogue,std::cin);
-//    jr.ReadTransportDataBase();
-//    renderer::MapRenderer map_renderer = jr.MapRenderFromJson();
-//    tr_catalogue::router::TransportRouter transport_router = jr.TransportRouterFromJson();
-//    tr_catalogue::RequestHandler request_handler(transport_catalogue, transport_router,map_renderer);
-//    jr.ResponseRequests(std::cout, request_handler);
-//}
-
 
 #include "json_reader.h"
 #include "transport_catalogue.h"
