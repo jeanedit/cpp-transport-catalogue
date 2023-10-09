9c25d7661e30c6e7dec4a70b0ac67769dff67979
Valery Kozhin <35922956+jeanedit@users.noreply.github.com>
2023-10-09 16:52:05 +0500

    15th sprint

--- transport-catalogue/src/transport_catalogue.h
+++ transport-catalogue/src/transport_catalogue.h
@@ -18,34 +18,28 @@ namespace tr_catalogue {
 
 	class TransportCatalogue {
 	public:
-
 		TransportCatalogue() = default;
 
 		void AddStop(domain::Stop&& stop);
-
 		const domain::Stop* FindStop(std::string_view stop_name) const;
-
 		void AddBus(domain::Bus&& bus);
-
 		const domain::Bus* FindBus(std::string_view bus_name) const;
 
-        inline const std::deque<domain::Bus>& GetAllBuses() const{
-            return buses_storage_;
-        }
+        	inline const std::deque<domain::Bus>& GetAllBuses() const{
+            	return buses_storage_;
+        	}
         
-        inline const std::deque<domain::Stop>& GetAllStops() const{
-            return stops_storage_;
-        }
+        	inline const std::deque<domain::Stop>& GetAllStops() const{
+            	return stops_storage_;
+        	}
         
 		void AddStopPairsDistances(const domain::Stop* from,const domain::Stop* to, int distance);
-
 		double ComputeGeoRouteLength(const domain::Bus* bus) const;
-
 		size_t ComputeActualRouteLength(const domain::Bus* bus) const;
         
-        const std::unordered_map<std::string_view, std::set<std::string_view>>& StopToBuses() const;
+        	const std::unordered_map<std::string_view, std::set<std::string_view>>& StopToBuses() const;
         
-        const std::deque<const domain::Stop*> StopsWithBus() const;
+        	const std::deque<const domain::Stop*> StopsWithBus() const;
 
 		size_t GetDistanceBetweenStops(const domain::Stop* from, const domain::Stop* to) const;
 
@@ -62,9 +56,7 @@ namespace tr_catalogue {
 		}
 
 	private:
-
 		std::deque<domain::Stop> stops_storage_;
-
 		std::deque<domain::Bus> buses_storage_;
 
 		std::unordered_map<std::string_view, const domain::Stop*> stops_;
